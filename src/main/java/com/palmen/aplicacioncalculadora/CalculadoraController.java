ce9b4ecd1b9b25299834701043a5b7ea20a4ae30
Pbl99 <pabloam1999@hotmail.com>
2024-04-28 10:38:56 +0200

    Creo tres procedimientos uno de @FXML para poder inicializar métodos.. y los procedimientos ponerNumeros y controlarBotonesOperadores

--- src/main/java/com/palmen/aplicacioncalculadora/CalculadoraController.java
+++ src/main/java/com/palmen/aplicacioncalculadora/CalculadoraController.java
@@ -71,4 +71,48 @@ public class CalculadoraController {
     @FXML
     private Label lblResultado;
 
+    @FXML
+    private void initialize() {
+        ponerNumeros();
+        controlarBotonesOperadores();
+    }
+
+    //Procedimiento para poner los numeros en el visor de la calculadora (lblResultado)
+    public void ponerNumeros() {
+        ArrayList<Button> botones = new ArrayList<>(Arrays.asList(btnNumero0, btnNumero1, btnNumero2, btnNumero3,
+                btnNumero4, btnNumero5, btnNumero6, btnNumero7, btnNumero8, btnNumero9));
+
+        for (Button boton : botones) {
+            boton.setOnMouseClicked(e -> {
+                String numero = boton.getText();
+                String textoActual = lblResultado.getText();
+
+                // Verifica si el texto actual es "0" y lo reemplaza por el número
+                if (textoActual.equals("0")) {
+                    lblResultado.setText(numero);
+                } else {
+                    lblResultado.setText(textoActual + numero);
+                }
+            });
+        }
+    }
+
+    //Procedimiento para poner los operadores en el visor de la calculadora (lblResultado)
+    public void controlarBotonesOperadores() {
+        ArrayList<Button> botones = new ArrayList<>(Arrays.asList(btnDivision, btnMultiplicacion, btnResta, btnSuma));
+
+        for (Button boton : botones) {
+            boton.setOnMouseClicked(e -> {
+                String operador = boton.getText();
+                String textoActual = lblResultado.getText();
+
+                // Verifica si el texto actual es un operador
+                if (textoActual.equals("÷") || textoActual.equals("x") || textoActual.equals("-") || textoActual.equals("+")) {
+                    lblResultado.setText(operador);
+                } else {
+                    lblResultado.setText(textoActual + " " + operador + " ");
+                }
+            });
+        }
+    }
 }
